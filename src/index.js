f48b7b22c14f75fc5b24579f195640c829540a22
Abhishek Choubey <choubeyabhishek53@gmail.com>
2022-05-01 08:23:36 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; //ES6
+// import ReactDOM from "react-dom"; //Has Render method.
+import { createRoot } from "react-dom/client"; //For React 18
+//CSS
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+  text: "I Love You to the Moon and Back",
+  authorName: "Amelia Hepworth and Tim Warnes",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/51FzVxSG6NL.jpg",
+  text: "Our Class is a Family",
+  authorName: "Shannon Olsen",
+};
+
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg";
+const text = "I Love You to the Moon and Back";
+const authorName = "Amelia Hepworth and Tim Warnes";
+
+function BookList() {
+  //traditional JS function
+
+  //now we return html i.e called JSX.
+  return (
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.text}
+        author={firstBook.authorName}
+      />
+
+<Book
+        img={firstBook.img}
+        title={firstBook.text}
+        author={firstBook.authorName}
+      />
+
+<Book
+        img={firstBook.img}
+        title={firstBook.text}
+        author={firstBook.authorName}
+      />
+
+<Book
+        img={firstBook.img}
+        title={firstBook.text}
+        author={firstBook.authorName}
+      />
+
+<Book
+        img={firstBook.img}
+        title={firstBook.text}
+        author={firstBook.authorName}
+      />
+
+
+<Book
+        img={firstBook.img}
+        title={firstBook.text}
+        author={firstBook.authorName}
+      />
+
+
+<Book
+        img={firstBook.img}
+        title={firstBook.text}
+        author={firstBook.authorName}
+      />
+  
+    </section>
+  );
+}
+
+const Book = (props) => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={props.img}></img>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+//ReactDOM.render(<BookList />, document.getElementById("root"));  //This for React 17
+
+// Deprecation notice: ReactDOM.render is no longer supported in React 18
+
+createRoot(document.getElementById("root")).render(<BookList />); //For React 18 Version
